dc7b8e7d2948dc186707136f2b3ce86081c6d8c7
MeissaMoon <69614784+MeissaMoon@users.noreply.github.com>
2021-01-25 21:56:20 +0900

    Delete leetcode 206. Reverse Linked List.py

--- leetcode 206. Reverse Linked List.py
+++
@@ -1,8 +0,0 @@
-    def reverseList(self, head: ListNode) -> ListNode:
-        node, prev = head, None     # node가 None이 되고 뒤집힌 prev가 첫번째 값 head가 될때 종료
-
-        while node:
-            next, node.next = node.next, prev   # node의 다음값과 이전 값을 비교하면서 뒤집는다?
-            prev, node = node, next
-
-        return prev
